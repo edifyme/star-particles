4bbeae760cbf2129739e24bfd32c135e4bcd3e88
edifyme <edifyme@users.noreply.github.com>
2022-03-24 18:44:25 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ function randInt(max) {
   return  Math.random() * max | 0;
 }
 
-// calculate between two points
+// calculate distance between two points
 function distance(x1, y1, x2, y2) {
   const dx = x2 - x1;
   const dy = y2 - y1;
